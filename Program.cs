7434fcbbc303f140dd4874816b0a545ce745f48e
Dubrov21 <vald5red@yandex.ru>
2023-10-06 20:33:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Написать программу, которая принимает число и выдает сумму цифр в числе
+
+using System;
+
+namespace SummaOfNumbersCollection
+{
+    class Program
+    {
+        static int GetSum(int num)
+        {
+            int i = 1;
+            int sum = 0;
+
+            while (i <= num)
+            {
+                sum += i;
+                i++;
+            }
+            return sum;
+        }
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = GetSum(num);
+            Console.WriteLine(result);
+        }
+    }
+}
